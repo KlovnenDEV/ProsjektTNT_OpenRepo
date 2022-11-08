ed7431f6e04e6341f4e88a4cbb737712dad92084
RainoDEV2 <113799393+RainoDEV2@users.noreply.github.com>
2022-11-08 03:34:12 +0100

    1.0 Stark EUP fra Cfx Forums

---
+++ resources/[Klaer]/RadioStrap/__resource.lua
@@ -0,0 +1,1 @@
+'author' Welder#6568
---
+++ resources/[Klaer]/RadioStrap/fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_pridemonth.meta',
+  'mp_f_freemode_01_mp_f_pridemonth.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_pridemonth.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_pridemonth.meta'
